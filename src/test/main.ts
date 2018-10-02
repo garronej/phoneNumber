41a5f396216ef2b2990d959250171387956cf97e
garronej <joseph.garrone.gj@gmail.com>
2018-10-02 12:21:35 +0200

    add some tests

--- src/test/main.ts
+++ src/test/main.ts
@@ -11,4 +11,10 @@ console.assert(phoneNumber.isDialable(ph));
 
 console.log( phoneNumber.prettyPrint(ph, "fr"));
 
+console.log( phoneNumber.build("06 36 78 63 85", "it") );
+
+console.log( phoneNumber.build("302 44444444", "fr") );
+
+console.log( phoneNumber.build("0033636786385", "it") );
+
 console.log("DONE");
